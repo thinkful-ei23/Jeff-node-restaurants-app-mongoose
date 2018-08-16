d1f982a64450f591faca3737548effe7bfda187f
Jeff Maxwell <maxjeffwell@gmail.com>
2018-08-15 21:02:51 -0400

    changed db url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://maxjeffwell:jeff123@ds121332.mlab.com:21332/restaurants-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
