b727119034c7658c9189e3d0c70940ca877e9c83
Jeff Maxwell <maxjeffwell@gmail.com>
2018-08-15 20:40:06 -0400

    modified config file

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://maxjeffwell:73Xs0YWE@ds121312.mlab.com:21312/restaurants-mongoose-db'
+	// 'mongodb://localhost/restaurants-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
